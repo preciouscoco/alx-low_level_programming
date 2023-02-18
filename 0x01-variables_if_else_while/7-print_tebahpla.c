83b74d24a6e0f4caaf8397548669bd091817cd72
preciouscoco <preciousanadu16@gmail.com>
2023-02-18 14:29:06 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{
+
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+
+{
+
+putchar(c);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
