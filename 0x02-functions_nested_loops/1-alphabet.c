6d57a73860cbc398b14987b3517e72dbd8bf1f65
preciouscoco <preciousanadu16@gmail.com>
2023-02-22 13:34:01 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+
+_putchar('\n');
+
+}
